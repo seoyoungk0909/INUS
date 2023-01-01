da99c79d05f369b7ee0ae9ff35fec0b5e4e32f51
seoyoungk0909 <skimbj@connect.ust.hk>
2023-01-01 21:10:36 +0900

    event detail

--- lib/views/event_detail_page.dart
+++ lib/views/event_detail_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../controllers/event_controller.dart';
 import '../models/event_model.dart';
+import 'package:aus/utils/color_utils.dart';
+import '../views/event_ui.dart';
 
 class EventDetailPage extends StatefulWidget {
   const EventDetailPage({Key? key, required this.title}) : super(key: key);
@@ -27,14 +29,95 @@ class EventDetailPageState extends State<EventDetailPage> {
         title: Text(widget.title),
         centerTitle: false,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(controller.event.title),
-          ],
-        ),
-      ),
+      body: eventDetailUI(context, controller),
+
+      // body: Center(child: eventDetailUI(context, controller)),
     );
   }
+}
+
+//event photo
+Widget eventDetailPhoto(BuildContext context, EventController controller) {
+  return Padding(
+    padding: const EdgeInsets.only(bottom: 20),
+    child: Container(
+      width: double.infinity,
+      height: 200,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(eventImage(controller.event.category)),
+          fit: BoxFit.cover,
+        ),
+      ),
+    ),
+  );
+}
+
+//event category hashtag
+Widget eventDetailCategory(
+    BuildContext context, String hexButtonColor, EventController controller) {
+  return Padding(
+    padding: const EdgeInsetsDirectional.fromSTEB(4, 0, 4, 4),
+    child: Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 12, 0),
+          child: Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(4, 0, 4, 0),
+            child: Text(
+              'View',
+              style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                    fontFamily: 'Outfit',
+                    color: hexStringToColor(hexButtonColor),
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal,
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 12, 0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(4, 0, 8, 0),
+                child: Text(
+                  'Comment',
+                  style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                        fontFamily: 'Outfit',
+                        color: hexStringToColor(hexButtonColor),
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                      ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget eventDetailUI(BuildContext context, EventController controller,
+    {Function? setState}) {
+  return SingleChildScrollView(
+    child: Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        eventDetailPhoto(context, controller),
+        Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(4, 0, 4, 4),
+          child: Column(
+            children: [
+              eventDetailCategory(context, "#AAAAAA", controller),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
 }
