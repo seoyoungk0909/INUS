a6419b13c94b68682f2873e76902025aac3509c5
seoyoungk0909 <skimbj@connect.ust.hk>
2023-01-01 23:55:47 +0900

    event detail page UI

--- lib/views/event_detail_page.dart
+++ lib/views/event_detail_page.dart
@@ -30,94 +30,258 @@ class EventDetailPageState extends State<EventDetailPage> {
         centerTitle: false,
       ),
       body: eventDetailUI(context, controller),
-
-      // body: Center(child: eventDetailUI(context, controller)),
     );
   }
+}
+
+//function that returns text
+Widget text(BuildContext context, EventController controller, String content,
+    double size,
+    {bool bold = false, String textColor = '#FFFFFF'}) {
+  if (bold == true) {
+    return Text(content,
+        style: Theme.of(context).textTheme.bodyText2?.copyWith(
+            fontFamily: 'Outfit',
+            color: hexStringToColor(textColor),
+            fontSize: size,
+            fontWeight: FontWeight.w600));
+  }
+  return Text(content,
+      style: Theme.of(context).textTheme.bodyText2?.copyWith(
+            fontFamily: 'Outfit',
+            color: hexStringToColor(textColor),
+            fontSize: size,
+          ));
 }
 
 //event photo
 Widget eventDetailPhoto(BuildContext context, EventController controller) {
   return Padding(
-    padding: const EdgeInsets.only(bottom: 20),
-    child: Container(
-      width: double.infinity,
-      height: 200,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(eventImage(controller.event.category)),
-          fit: BoxFit.cover,
+    padding: const EdgeInsets.only(bottom: 12),
+    child: Expanded(
+      child: Container(
+        width: double.infinity,
+        height: 200,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(eventImage(controller.event.category)),
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     ),
   );
 }
 
-//event category hashtag
-Widget eventDetailCategory(
-    BuildContext context, String hexButtonColor, EventController controller) {
+//category hashtag
+Widget categoryHashtag(BuildContext context, EventController controller) {
   return Padding(
-    padding: const EdgeInsetsDirectional.fromSTEB(4, 0, 4, 4),
-    child: Row(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Padding(
-          padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 12, 0),
-          child: Padding(
-            padding: const EdgeInsetsDirectional.fromSTEB(4, 0, 4, 0),
-            child: Text(
-              'View',
-              style: Theme.of(context).textTheme.bodyText2?.copyWith(
-                    fontFamily: 'Outfit',
-                    color: hexStringToColor(hexButtonColor),
-                    fontSize: 14,
-                    fontWeight: FontWeight.normal,
-                  ),
-            ),
-          ),
+    padding: const EdgeInsets.only(top: 14),
+    child: Expanded(
+      child: Container(
+        height: 22,
+        decoration: BoxDecoration(
+          color: hexStringToColor("#3E3E3E"),
+          borderRadius: BorderRadius.circular(10),
         ),
-        Padding(
-          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 12, 0),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(4, 0, 8, 0),
-                child: Text(
-                  'Comment',
-                  style: Theme.of(context).textTheme.bodyText2?.copyWith(
-                        fontFamily: 'Outfit',
-                        color: hexStringToColor(hexButtonColor),
-                        fontSize: 14,
-                        fontWeight: FontWeight.normal,
-                      ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
+        child: Flexible(
+            child: Container(
+                padding: const EdgeInsetsDirectional.fromSTEB(9, 5, 9, 0),
+                child: text(context, controller, controller.event.tag, 12))),
+      ),
     ),
   );
 }
 
+//Quick view
+Widget quickView(BuildContext context, EventController controller) {
+  return Padding(
+    padding: const EdgeInsets.only(top: 18),
+    child: Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: hexStringToColor("#3E3E3E"),
+        borderRadius: BorderRadius.circular(6),
+      ),
+      child: Expanded(
+        child: Flexible(
+            child: Container(
+                padding: const EdgeInsetsDirectional.fromSTEB(17, 22, 17, 22),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    text(context, controller, 'Quick View', 15, bold: true),
+                    Padding(
+                        padding: const EdgeInsets.only(top: 15),
+                        child: Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 4.0),
+                                  child: text(context, controller, 'Date', 14,
+                                      textColor: '#AAAAAA'),
+                                ),
+                                text(context, controller, controller.event.date,
+                                    15,
+                                    bold: true),
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 100),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(bottom: 4.0),
+                                    child: text(context, controller, 'Time', 14,
+                                        textColor: '#AAAAAA'),
+                                  ),
+                                  text(context, controller,
+                                      controller.event.time, 15,
+                                      bold: true),
+                                ],
+                              ),
+                            )
+                          ],
+                        ))
+                  ],
+                ))),
+      ),
+    ),
+  );
+}
+
+//event description
+Widget eventDescription(BuildContext context, EventController controller) {
+  return Padding(
+    padding: const EdgeInsets.only(top: 18),
+    child: Expanded(
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+              child:
+                  text(context, controller, controller.event.description, 14)),
+        ],
+      ),
+    ),
+  );
+}
+
+//detailed view
+Widget detailedView(BuildContext context, EventController controller) {
+  return Padding(
+      padding: const EdgeInsets.only(top: 18),
+      child: Expanded(
+        child: Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: hexStringToColor("#3E3E3E"),
+              borderRadius: BorderRadius.circular(6),
+            ),
+            child: Flexible(
+                child: Container(
+                    padding:
+                        const EdgeInsetsDirectional.fromSTEB(17, 22, 17, 22),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        text(context, controller, 'Detailed View', 15,
+                            bold: true),
+                        //date
+                        Padding(
+                          padding: const EdgeInsets.only(top: 15),
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(right: 53),
+                                child: text(context, controller, 'Date', 14,
+                                    textColor: '#AAAAAA'),
+                              ),
+                              text(context, controller, controller.event.date,
+                                  15,
+                                  bold: true),
+                            ],
+                          ),
+                        ),
+                        //time
+                        Padding(
+                          padding: const EdgeInsets.only(top: 15),
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(right: 52),
+                                child: text(context, controller, 'Time', 14,
+                                    textColor: '#AAAAAA'),
+                              ),
+                              text(context, controller, controller.event.time,
+                                  15,
+                                  bold: true),
+                            ],
+                          ),
+                        ),
+                        //language
+                        Padding(
+                          padding: const EdgeInsets.only(top: 15),
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(right: 20),
+                                child: text(context, controller, 'Language', 14,
+                                    textColor: '#AAAAAA'),
+                              ),
+                              text(context, controller,
+                                  controller.event.language, 15,
+                                  bold: true),
+                            ],
+                          ),
+                        ),
+                        //location
+                        Padding(
+                          padding: const EdgeInsets.only(top: 15),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(right: 28),
+                                child: text(context, controller, 'Location', 14,
+                                    textColor: '#AAAAAA'),
+                              ),
+                              Expanded(
+                                child: text(context, controller,
+                                    controller.event.location, 15,
+                                    bold: true),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )))),
+      ));
+}
+
 Widget eventDetailUI(BuildContext context, EventController controller,
     {Function? setState}) {
-  return SingleChildScrollView(
-    child: Column(
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        eventDetailPhoto(context, controller),
-        Padding(
-          padding: const EdgeInsetsDirectional.fromSTEB(4, 0, 4, 4),
-          child: Column(
-            children: [
-              eventDetailCategory(context, "#AAAAAA", controller),
-            ],
-          ),
+  return ListView(
+    children: [
+      eventDetailPhoto(context, controller),
+      Padding(
+        padding: const EdgeInsetsDirectional.fromSTEB(15, 0, 15, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            categoryButton(context, controller),
+            eventTitle(context, controller),
+            categoryHashtag(context, controller),
+            quickView(context, controller),
+            eventDescription(context, controller),
+            detailedView(context, controller),
+          ],
         ),
-      ],
-    ),
+      ),
+    ],
   );
 }
