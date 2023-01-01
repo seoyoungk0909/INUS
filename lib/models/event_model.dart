c29598806e3b691d4db112b54249b350a5812a0a
seoyoungk0909 <skimbj@connect.ust.hk>
2023-01-01 23:53:35 +0900

    add extra model data

--- lib/models/event_model.dart
+++ lib/models/event_model.dart
@@ -11,16 +11,34 @@ class Event {
   String title = "Tissue-engineering Integrated";
   String category = "Event";
   String tag = "#environment #education";
+  String description =
+      "Since their first direct discovery in 2015, gravitational waves have contributed significantly to knowledge about astrophysics and fundamental physics. This talk will first introduce the Open... ";
+  String language = "English, Cantonese";
+  String location =
+      "IAS4042, 4/F, Lo Ka Chung Building, Lee Shau Kee Campus, HKUST";
+  String date = "14 Dec 2022";
+  String time = "15:40";
   late DateTime timestamp;
 
   Event({
     String? eventTitle,
     String? eventCategory,
     String? eventTag,
+    String? eventDescription,
+    String? eventLanguage,
+    String? eventLocation,
+    String? eventDate,
+    String? eventTime,
   }) {
     title = eventTitle ?? title;
     category = eventCategory ?? category;
     tag = eventTag ?? tag;
+    description = eventDescription ?? description;
+    language = eventLanguage ?? language;
+    location = eventLocation ?? location;
+    date = eventDate ?? date;
+    time = eventTime ?? time;
+
     timestamp = DateTime.now();
   }
 }
