0f27dae1d55daba08d624e5c55965ea9b590d22f
Sehyun Choi <choisehyun98@gmail.com>
2023-01-23 23:26:44 +0900

    keyboard based bottom padding amount

--- lib/views/components/comment_box.dart
+++ lib/views/components/comment_box.dart
@@ -1,6 +1,8 @@
+import 'package:aus/utils/color_utils.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 
 import '../../controllers/post_controller.dart';
 
@@ -23,42 +25,50 @@ class CommentBoxState extends State<CommentBox> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        // margin: EdgeInsets.only(top: 8),
-        padding: const EdgeInsets.all(15),
-        child: Row(
-          children: [
-            Expanded(
-              child: TextField(
-                decoration: const InputDecoration(
-                  // labelText: 'Write your comment',
-                  hintText: 'Write your comment',
-                  border: InputBorder.none,
+    return KeyboardVisibilityBuilder(builder: (context, isKeyboardVisible) {
+      double bottomPad = isKeyboardVisible ? 0 : 30;
+      return Container(
+          // margin: EdgeInsets.only(top: 8),
+          padding: EdgeInsets.fromLTRB(15, 0, 15, bottomPad),
+          decoration: BoxDecoration(
+              color: Theme.of(context).primaryColorDark,
+              border: BorderDirectional(
+                  top: BorderSide(color: ApdiColors.lineGrey))),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  decoration: const InputDecoration(
+                    // labelText: 'Write your comment',
+                    hintText: 'Write your comment',
+                    border: InputBorder.none,
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      userEnterMessage = value;
+                    });
+                  },
                 ),
-                onChanged: (value) {
-                  setState(() {
-                    userEnterMessage = value;
-                  });
-                },
               ),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(primary: Colors.blue),
-              onPressed: () {
-                if (userEnterMessage.trim().isEmpty) {
-                  return;
-                }
-                FirebaseFirestore.instance.collection('post_comment').add({
-                  'body': userEnterMessage,
-                  'writerFlag': isWriter(),
-                  'time': DateTime.now(),
-                }).then((DocumentReference newComment) {
-                  widget.controller.addComment(newComment);
-                });
-              },
-              child: const Text("Post"),
-            ),
-          ],
-        ));
+              TextButton(
+                style: TextButton.styleFrom(primary: Colors.blue),
+                onPressed: userEnterMessage.trim().isEmpty
+                    ? null
+                    : () {
+                        FirebaseFirestore.instance
+                            .collection('post_comment')
+                            .add({
+                          'body': userEnterMessage,
+                          'writerFlag': isWriter(),
+                          'time': DateTime.now(),
+                        }).then((DocumentReference newComment) {
+                          widget.controller.addComment(newComment);
+                        });
+                      },
+                child: const Text("Post"),
+              ),
+            ],
+          ));
+    });
   }
 }
