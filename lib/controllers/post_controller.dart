b9a668ac88753dee29b9fb1a5046a8ee1855b4fc
seoyoungk0909 <skimbj@connect.ust.hk>
2023-04-25 15:59:06 +0800

    point system

--- lib/controllers/post_controller.dart
+++ lib/controllers/post_controller.dart
@@ -7,21 +7,29 @@ class PostController {
   void incrementView() {
     post.views++;
     post.firebaseDocRef?.update({"viewCount": FieldValue.increment(1)});
+    post.points++;
+    post.firebaseDocRef?.update({"points": FieldValue.increment(1)});
   }
 
   void incrementReport() {
     post.reportCount++;
     post.firebaseDocRef?.update({"reportCount": FieldValue.increment(1)});
+    post.points -= 20;
+    post.firebaseDocRef?.update({"points": FieldValue.increment(-20)});
   }
 
   void postSave() {
     post.saveCount++;
     post.firebaseDocRef?.update({"saveCount": FieldValue.increment(1)});
+    post.points += 10;
+    post.firebaseDocRef?.update({"points": FieldValue.increment(10)});
   }
 
   void postSaveCancel() {
     post.saveCount--;
     post.firebaseDocRef?.update({"saveCount": FieldValue.increment(-1)});
+    post.points -= 10;
+    post.firebaseDocRef?.update({"points": FieldValue.increment(-10)});
   }
 
   void addComment(DocumentReference newComment) {
@@ -29,6 +37,8 @@ class PostController {
       'comments': FieldValue.arrayUnion([newComment])
     });
     post.commentRefs.add(newComment);
+    post.points += 5;
+    post.firebaseDocRef?.update({"points": FieldValue.increment(5)});
   }
 
   void addReport(DocumentReference newReport) {
@@ -36,6 +46,23 @@ class PostController {
       'reports': FieldValue.arrayUnion([newReport])
     });
   }
+
+  // void updatePoints() {
+  // int views = post.views;
+  // int comments = 5 * post.numComments();
+  // int save = 10 * post.saveCount;
+
+  // int reportCount = 20 * post.reportCount;
+  // int category = (post.category == '19+') ? 50 : 0;
+  // Duration diff = DateTime.now().difference(post.timestamp);
+  // int dayPercent = (1 - (0.01 * diff.inDays)) as int;
+  // int point = (post.points) * dayPercent;
+
+  // // int point =
+  // //     ((views + comments + save) - (reportCount + category)) * dayPercent;
+  // post.points = point;
+  // post.firebaseDocRef?.update({"points": point});
+  // }
 
   void lazyDeletePost() {
     post.deleted = true;
